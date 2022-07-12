50c1bb92bdf3e0b682728ec6d5634559c97138f3
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-12 13:22:42 -0700

    Coverage and weights

---
+++ src/generateItem.js
@@ -0,0 +1,56 @@
+import { getRandomInt, getRandomIntInRange }  from "./utils/helper";
+
+const DAMAGE_TYPES = ["piercing", "slashing", "bludgeoning"];
+const ARMOR_TYPES = ["light", "medium", "heavy"];
+
+export function generateStat(rarity) {
+  let stat = 0;
+  if (rarity === "epic") {
+    stat = getRandomInt(8, 20);
+  } else if (rarity === "rare") {
+    stat = getRandomInt(5, 15);
+  } else {
+    stat = getRandomInt(1, 10);
+  }
+  return stat;
+}
+
+export function generateBaseWeapon() {
+  return {
+    type: "weapon",
+    damageType: DAMAGE_TYPES[getRandomInt(DAMAGE_TYPES.length)],
+    damage: 0,
+    attackSpeed: 0,
+  };
+}
+
+export function generateBaseArmor() {
+  return {
+    type: "armor",
+    defense: 0,
+    armorType: ARMOR_TYPES[getRandomInt(ARMOR_TYPES.length)],
+  };
+}
+
+export function generateItem(itemOptions = {}) {
+  const { type, rarity } = itemOptions;
+
+  if (!type) {
+    return null;
+  }
+
+  const baseItem =
+    type === "weapon" ? generateBaseWeapon() : generateBaseArmor();
+
+  baseItem.rarity = rarity || "common";
+
+  if (baseItem.type === "weapon") {
+    baseItem.damage = generateStat(rarity);
+  } else {
+    baseItem.defense = generateStat(rarity);
+    // This line is "covered" but we don't actually know if "heavy" gets true
+    baseItem.hasStealthDisadvantage = baseItem.armorType === "heavy";
+    baseItem.stealthBonus = baseItem.armorType === "light" ? 3 : 0;
+  }
+  return baseItem;
+}
